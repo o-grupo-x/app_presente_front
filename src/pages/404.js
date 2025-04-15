359ee2af8257cbac79f5a69f54935b00a52c1873
Diuary Machado <104037870+Diuary-Machado@users.noreply.github.com>
2025-04-15 19:32:46 -0300

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import styles from './style.module.css'
+import styles from './style.module.css';
 
 export default function Custom404() {
   return (
-    <div className={styles.containerError}>
-      <h1 className={styles.error404}>404 - Page Not Found</h1>
-    </div>
+    <main className={styles.containerError}>
+      <h1 className={styles.error404}>404</h1>
+      <h2 className={styles.message}>Página não encontrada</h2>
+      <p className={styles.description}>
+        Desculpe, a página que você procura não existe ou foi movida.
+      </p>
+    </main>
   );
 }
