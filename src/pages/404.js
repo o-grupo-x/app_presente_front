6e2172c02853fd68210ab8b1d1a14f30c010ca8d
wander <guilhermenarde70@gmail.com>
2025-05-11 19:18:34 -0400

    Update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import styles from './style.module.css';
+import styles from './style.module.css'
 
 export default function Custom404() {
   return (
-    <div className={styles.container}>
-      <div className={styles.stars}></div>
-      <h1 className={styles.errorCode}>404</h1>
-      <h2 className={styles.errorMessage}>Page Not Found</h2>
-      <p className={styles.description}>
-        Oops! It looks like you're lost in space. The page you're looking for doesn't exist.
-      </p>
-      <a href="/" className={styles.homeLink}>
-        Return to Home
-      </a>
+    <div className={styles.containerError}>
+      <h1 className={styles.error404}>404 - Page Not Found</h1>
     </div>
   );
 }
