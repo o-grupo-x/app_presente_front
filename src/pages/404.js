8fd288af5ca10cdfd8c22f58f6be07a9595c0f10
wander <guilhermenarde70@gmail.com>
2025-05-11 23:00:54 -0400

    Update: 404 add a test for test

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,8 @@ import styles from './style.module.css'
 export default function Custom404() {
   return (
     <div className={styles.containerError}>
-      <h1 className={styles.error404}>404 - Page Not Found </h1>
-      <h2 className={styles.error404}>Pagina errada fiote, vai tomar no cu </h2>       
+      <h1 className={styles.error404}>404 - Page Not Found teste  </h1>
+
     </div>
   );
 }
