17bccb3bf44023cc33c130a2bffacbbfe8c09acf
Guilherme-Denarde <109321031+Guilherme-Denarde@users.noreply.github.com>
2025-05-12 23:40:04 -0300

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import styles from './style.module.css'
 export default function Custom404() {
   return (
     <div className={styles.containerError}>
-      <h1 className={styles.error404}>404 - Page Not Found teste  </h1>
+      <h1 className={styles.error404}>404 - Page Not Found teste novo  </h1>
 
     </div>
   );
