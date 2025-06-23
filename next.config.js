7d0fe7d5163584cf3e3a0e66e1dbd8928d1a4ba9
Saler C. Lourenço <salerclourenco@gmail.com>
2025-06-23 00:03:29 -0300

    adiciona redireciomento da requisição para o backend no next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,31 @@
-const path = require('path');
-require('dotenv').config();
+const path = require("path");
+require("dotenv").config();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 
+  // 🚀 Adiciona rewrites para redirecionar chamadas de API
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://app-presente-back-service.stage-app-chamada-production.svc.cluster.local:8000/api/:path*", // atualize com o IP correto
+      },
+    ];
+  },
+
   webpack: (config, { isServer }) => {
     // Adicionando aliases para simplificar os imports
-    config.resolve.alias['@'] = path.join(__dirname, 'src'); // Alias para a pasta src
-    config.resolve.alias['@components'] = path.join(__dirname, 'src', 'components'); // Alias para componentes
-    config.resolve.alias['@contexts'] = path.join(__dirname, 'src', 'contexts'); // Alias para contextos
-    config.resolve.alias['@utils'] = path.join(__dirname, 'src', 'utils'); // Alias para utilitários
-    config.resolve.alias['@client'] = path.join(__dirname, 'src', 'client'); // Alias para a camada de client API
+    config.resolve.alias["@"] = path.join(__dirname, "src"); // Alias para a pasta src
+    config.resolve.alias["@components"] = path.join(
+      __dirname,
+      "src",
+      "components"
+    ); // Alias para componentes
+    config.resolve.alias["@contexts"] = path.join(__dirname, "src", "contexts"); // Alias para contextos
+    config.resolve.alias["@utils"] = path.join(__dirname, "src", "utils"); // Alias para utilitários
+    config.resolve.alias["@client"] = path.join(__dirname, "src", "client"); // Alias para a camada de client API
 
     // Configurações para fallback de módulos não disponíveis no servidor
     if (!isServer) {
